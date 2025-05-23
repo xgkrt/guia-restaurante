e4d4be4d50af1f45287ac40b63e19ce738d50250
xgkrt <lucas.gkrt@gmail.com>
2025-05-23 09:05:04 -0300

    implementando o postmappig para sempre que for a url correta ir para o tiporestaurante

--- src/main/java/br/senac/sp/guiarestaurante/controller/TipoRestauranteController.java
+++ src/main/java/br/senac/sp/guiarestaurante/controller/TipoRestauranteController.java
@@ -1,13 +1,36 @@
 package br.senac.sp.guiarestaurante.controller;
 
 import br.senac.sp.guiarestaurante.model.TipoRestaurante;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.senac.sp.guiarestaurante.repository.TipoRestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
+@RequestMapping("/tiporestaurante")
 public class TipoRestauranteController {
 
-    @GetMapping("/teste")
+    @Autowired //injeção de dependencias
+    private TipoRestauranteRepository tipoRestauranteRepository;
+
+    @PostMapping
+    public ResponseEntity<Object> criarTipo(@RequestBody TipoRestaurante tipo){
+        try{
+            //salvar no banco
+            tipoRestauranteRepository.save(tipo);
+            //retornar resposta
+            return ResponseEntity.created(URI.create("/tiporestaurante/" + tipo.getId())).body(tipo);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            String erro = e.getMessage();
+            return new ResponseEntity<Object>(erro, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
     public TipoRestaurante getRestaurante(){
         TipoRestaurante tipoRestaurante = new TipoRestaurante();
         tipoRestaurante.setId(1L);
