666c7e6287bd0e9ddb07064b0271bf71adf322f6
xgkrt <lucas.gkrt@gmail.com>
2025-05-23 08:50:28 -0300

    adicionando o repository para o crud de tiporestaurante

---
+++ src/main/java/br/senac/sp/guiarestaurante/repository/TipoRestauranteRepository.java
@@ -0,0 +1,7 @@
+package br.senac.sp.guiarestaurante.repository;
+
+import br.senac.sp.guiarestaurante.model.TipoRestaurante;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TipoRestauranteRepository extends CrudRepository<TipoRestaurante, Long> {
+}
