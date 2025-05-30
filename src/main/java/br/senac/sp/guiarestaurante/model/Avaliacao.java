4abf831d7d1a385c419379842a40782f3c7b9e38
xgkrt <lucas.gkrt@gmail.com>
2025-05-30 10:26:42 -0300

    adicionando jsonformat para data e para somente escrever

--- src/main/java/br/senac/sp/guiarestaurante/model/Avaliacao.java
+++ src/main/java/br/senac/sp/guiarestaurante/model/Avaliacao.java
@@ -1,21 +1,29 @@
 package br.senac.sp.guiarestaurante.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.util.Calendar;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
 @Data
 @Entity
 public class Avaliacao {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private double nota;
-    private String comentario;
-    private Calendar dataVisita;
-    @ManyToOne
-    private Restaurante restaurante;
-    @ManyToOne
-    private Usuario usuario;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private double nota;
+	private String comentario;
+	@JsonFormat(pattern = "dd-MM-yyyy")
+	private Calendar dataVisita;
+	@ManyToOne
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	private Restaurante restaurante;
+	@ManyToOne
+	private Usuario usuario;
 }
