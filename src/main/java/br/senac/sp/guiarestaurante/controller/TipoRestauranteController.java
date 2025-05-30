9e9d2e738c5a69808d5f427aa2ced795f7d5878a
xgkrt <lucas.gkrt@gmail.com>
2025-05-30 10:29:42 -0300

    alterando variaveis

--- src/main/java/br/senac/sp/guiarestaurante/controller/TipoRestauranteController.java
+++ src/main/java/br/senac/sp/guiarestaurante/controller/TipoRestauranteController.java
@@ -1,73 +1,76 @@
 package br.senac.sp.guiarestaurante.controller;
 
-import br.senac.sp.guiarestaurante.model.TipoRestaurante;
-import br.senac.sp.guiarestaurante.repository.TipoRestauranteRepository;
+import java.net.URI;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
+import br.senac.sp.guiarestaurante.model.TipoRestaurante;
+import br.senac.sp.guiarestaurante.repository.TipoRestauranteRepository;
 
 @RestController
 @RequestMapping("/tiporestaurante")
 public class TipoRestauranteController {
+	@Autowired
+	private TipoRestauranteRepository repository;
 
-    @Autowired //injeção de dependencias
-    private TipoRestauranteRepository tipoRestauranteRepository;
+	@PostMapping
+	public ResponseEntity<Object> criarTipo(@RequestBody TipoRestaurante tipo) {
+		try {
+			// salvar no banco
+			repository.save(tipo);
+			// retorna a resposta
+			return ResponseEntity.created(URI.create("/tiporestaurante/" + tipo.getId())).body(tipo);
+		} catch (Exception e) {
+			e.printStackTrace();
+			String erro = e.getMessage();
+			return new ResponseEntity<Object>(erro, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-    @PostMapping
-    public ResponseEntity<Object> criarTipo(@RequestBody TipoRestaurante tipo){
-        try{
-            //salvar no banco
-            tipoRestauranteRepository.save(tipo);
-            //retornar resposta
-            return ResponseEntity.created(URI.create("/tiporestaurante/" + tipo.getId())).body(tipo);
+	@PutMapping("/{id}")
+	public ResponseEntity<Object> alterarTipo(@RequestBody TipoRestaurante tipo, @PathVariable("id") Long id) {
+		try {
+			// salvar no banco
+			repository.save(tipo);
+			// retorna a resposta
+			return ResponseEntity.ok().build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			String erro = e.getMessage();
+			return new ResponseEntity<Object>(erro, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            String erro = e.getMessage();
-            return new ResponseEntity<Object>(erro, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+		repository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> alterarTipo(@RequestBody TipoRestaurante tipo, @PathVariable("id") Long id){
-        try{
-            //salvar no banco
-            tipoRestauranteRepository.save(tipo);
-            //retornar resposta
-            return ResponseEntity.ok().build();
+	@GetMapping
+	public ResponseEntity<Iterable<TipoRestaurante>> listarTodos() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            String erro = e.getMessage();
-            return new ResponseEntity<Object>(erro, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletar(@PathVariable("id") Long id){
-        tipoRestauranteRepository.deleteById(id);
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<Iterable<TipoRestaurante>> listarTodos(){
-        return ResponseEntity.ok(tipoRestauranteRepository.findAll());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<TipoRestaurante> buscarId(@PathVariable Long id){
-        Optional<TipoRestaurante> tipo = tipoRestauranteRepository.findById(id);
-        if (tipo.isPresent()){
-            return ResponseEntity.ok(tipo.get());
-        }else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<TipoRestaurante> buscarPorId(@PathVariable("id") Long id) {
+		Optional<TipoRestaurante> tipo = repository.findById(id);
+		if (tipo.isPresent()) {
+			return ResponseEntity.ok(tipo.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 
 }
--- src/main/java/br/senac/sp/guiarestaurante/model/TipoRestaurante.java
+++ src/main/java/br/senac/sp/guiarestaurante/model/TipoRestaurante.java
@@ -1,21 +1,22 @@
 package br.senac.sp.guiarestaurante.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Setter;
 
 @Data
 @Entity
-@Setter
 public class TipoRestaurante {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-
-    private Long id;
-    @Column(unique = true)
-    private String nome;
-    private String descricao;
-    private String palavrasChave;
+	@Id
+	@GeneratedValue
+	(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true)
+	private String nome;
+	private String descricao;
+	private String palavrasChave;
 
 }
