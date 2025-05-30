9de4128e20dccf70ccf918b6a8e654d3c3a889d6
xgkrt <lucas.gkrt@gmail.com>
2025-05-30 10:24:39 -0300

    alterando variaveis

--- src/main/java/br/senac/sp/guiarestaurante/config/AppConfig.java
+++ src/main/java/br/senac/sp/guiarestaurante/config/AppConfig.java
@@ -1,5 +1,7 @@
 package br.senac.sp.guiarestaurante.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -7,32 +9,28 @@ import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/guiarestaurante");
-        driverManagerDataSource.setUsername("lucas");
-        driverManagerDataSource.setPassword("");
-
-        return driverManagerDataSource;
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter(){
-        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setDatabase(Database.MYSQL);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
-        adapter.setPrepareConnection(true);
-        adapter.setGenerateDdl(true);
-        adapter.setShowSql(true);
-
-        return adapter;
-    }
-
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/guiarestaurante");
+		ds.setUsername("root");
+		ds.setPassword("");
+		return ds;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+		adapter.setPrepareConnection(true);
+		adapter.setGenerateDdl(true);
+		adapter.setShowSql(true);
+		return adapter;
+	}
+	
 }
