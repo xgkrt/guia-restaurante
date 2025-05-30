f0b8f71023ac3b8dac5d4bf9ca009d7b2a423f87
xgkrt <lucas.gkrt@gmail.com>
2025-05-30 10:27:43 -0300

    adicionando AvaliacaoRepository

---
+++ src/main/java/br/senac/sp/guiarestaurante/repository/AvaliacaoRepository.java
@@ -0,0 +1,7 @@
+package br.senac.sp.guiarestaurante.repository;
+
+import br.senac.sp.guiarestaurante.model.Avaliacao;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AvaliacaoRepository extends CrudRepository<Avaliacao,Long> {
+}
