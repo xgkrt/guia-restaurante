6e18dd39b126d43ffb3868bf526f5171adc27db9
xgkrt <lucas.gkrt@gmail.com>
2025-05-30 10:30:05 -0300

    adicionando query

--- src/main/java/br/senac/sp/guiarestaurante/model/Usuario.java
+++ src/main/java/br/senac/sp/guiarestaurante/model/Usuario.java
@@ -1,17 +1,21 @@
 package br.senac.sp.guiarestaurante.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class Usuario {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
-    @Column(unique = true)
-    private String email;
-    private String senha;
-
+	@Id
+	@GeneratedValue
+	(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	@Column(unique = true)
+	private String email;
+	private String senha;
 }
--- src/main/java/br/senac/sp/guiarestaurante/repository/TipoRestauranteRepository.java
+++ src/main/java/br/senac/sp/guiarestaurante/repository/TipoRestauranteRepository.java
@@ -1,8 +1,14 @@
 package br.senac.sp.guiarestaurante.repository;
 
-import br.senac.sp.guiarestaurante.model.TipoRestaurante;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.senac.sp.guiarestaurante.model.TipoRestaurante;
 
 public interface TipoRestauranteRepository extends CrudRepository<TipoRestaurante, Long> {
-
+	@Query("select t from TipoRestaurante t where t.palavrasChave like %:p% order by t.nome")
+	List<TipoRestaurante> buscarPorChave(@Param("p") String palavras);
 }
